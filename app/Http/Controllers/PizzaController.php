1c0e5303562bd047d9d9e7cc3470e119d2624900
ico <francisco100eg@gmail.com>
2023-10-06 18:45:13 -0300

    Feat: Added Inertia conection

--- app/Http/Controllers/PizzaController.php
+++ app/Http/Controllers/PizzaController.php
@@ -3,8 +3,17 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\Pizza;
+use Inertia\Inertia;
 
 class PizzaController extends Controller
 {
-    //
+    public function index()
+    {
+       $pizzas = Pizza::all();
+
+       return Inertia::render('Pizzas/All', [
+           'pizzas' => $pizzas
+       ]);
+    }
 }
